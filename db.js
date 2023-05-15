26d6e61f00b9a899e21e7673ebc2eb9202d021dc
sueys <sueys@live.fr>
2023-05-15 18:41:58 +0200

    first-c add connection db et installation dotenv

---
+++ db.js
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+import mysql from 'mysql';
+
+dotenv.config();
+export const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+});
+
+db.connect((err)=>{
+    if(err){
+        console.error('Erreur de connexion à la base de données MySQL :', err);
+    } else {
+        console.log('Connecté à la base de données MySQL');
+    }
+});
