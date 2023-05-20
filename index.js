38a3d92e7a9af0e8a8f041297ba2cd1733126293
sueys <sueys@live.fr>
2023-05-20 21:04:28 +0200

    add route and remove hard coded data

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors"
+import { db } from "./db.js"
 const app = express()
 
 
@@ -7,18 +8,30 @@ app.use(express.json())
 app.use(cors())
 
 app.get("/quiz", (req, res) => {
-    const data = [
-        {id: 0,slug:'premier',title: 'Premier Quiz',color:'rgba(104, 219, 255, 1)'},
-        {id: 1,slug:'deuxieme',title: 'Deuxième Quiz',color:'rgba(185, 117, 239, 1)'},
-        {id: 2,slug:'troisieme',title: 'Troisième Quiz',color:'rgba(252, 129, 77, 1)'},
-        {id: 3,slug:'quatrieme',title: 'Quatrième Quiz',color:'rgba(255, 213, 64, 1)'},
-        {id: 4,slug:'cinquieme',title: 'Cinquième Quiz',color:'rgba(255, 88, 238, 1)'},
-        {id: 5,slug:'sixieme',title: 'Sixième Quiz',color:'rgba(42, 237, 120, 1)'},
-    ]
+    db.query("SELECT * FROM quiz", (err, result) => {
+        if (err) {
+            // Gérez l'erreur de requête à la base de données
+            console.error(err);
+            res.status(500).json({ error: "Erreur lors de la récupération des quiz" });
+        } else {
 
-    // Envoyer la liste des quiz en réponse
-    res.json(data);
+            // Envoyer la liste des quiz en réponse
+            res.json(result);
+        }
+    })
 });
+
+app.get("/quiz/:slug", (req,res)=>{
+    console.log(req.params.slug)
+    db.query(`SELECT * FROM quiz WHERE slug = '${req.params.slug}'`,(err,result)=>{
+        if(err){
+            console.error(err);
+            res.status(500).json({ error: "Erreur lors de la récupération d'un quiz"})
+        } else {
+            res.json(result[0])
+        }
+    })
+})
 
 
 app.listen(8800, () => {
