0cf4aacdb7b37ee7f00394d78a6ed79bedc6721a
sueys <sueys@live.fr>
2023-05-22 13:28:01 +0200

    add utf8mb4

--- db.js
+++ db.js
@@ -7,6 +7,7 @@ export const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
+    charset:"utf8mb4"
 });
 
 db.connect((err)=>{
