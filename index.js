cc1e2c4a2ef88195710b8aa4e43721df70745e86
sueys <sueys@live.fr>
2023-06-03 12:13:46 +0200

    code clean up

--- index.js
+++ index.js
@@ -2,25 +2,21 @@ import express from "express";
 import cors from "cors"
 import { db } from "./db.js"
 
-
-const app = express()
-
-const errorHandling = (res,error,errorMessage="an error has occurred") => {
+const errorHandling = (res, error, errorMessage = "an error has occurred") => {
     const errorTime = new Date().getTime();
     console.error(errorTime, error);
     res.status(500).json({ error: errorMessage, errorTime })
-
 }
 
+const app = express()
 
 app.use(express.json())
 app.use(cors())
 
-
 app.get("/quiz", (req, res) => {
     db.query("SELECT * FROM quiz", (err, result) => {
         if (err) {
-            errorHandling(res,err,"Erreur lors de la récupération des quiz");
+            errorHandling(res, err, "Erreur lors de la récupération des quiz");
         } else {
             res.json(result);
         }
@@ -31,16 +27,13 @@ app.get("/quiz/:slug", (req, res) => {
     console.log(req.params.slug)
     db.query('SELECT * FROM quiz WHERE slug = ?', [req.params.slug], (err, result) => {
         if (err) {
-            errorHandling(res,err,"Erreur lors de la récupération d'un quiz");
+            errorHandling(res, err, "Erreur lors de la récupération d'un quiz");
 
         } else {
             res.json(result[0])
         }
     })
 });
-
-
-
 
 app.listen(8800, () => {
     console.log("Connected")
