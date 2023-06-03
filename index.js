d4278a3e80357a5f93533d8af4d446169b373ba9
sueys <sueys@live.fr>
2023-06-03 11:45:08 +0200

    add function errorhandling

--- index.js
+++ index.js
@@ -1,37 +1,45 @@
 import express from "express";
 import cors from "cors"
-import { db }  from "./db.js"
+import { db } from "./db.js"
+
+
 const app = express()
+
+const errorHandling = (res,error,errorMessage="an error has occurred") => {
+    const errorTime = new Date().getTime();
+    console.error(errorTime, error);
+    res.status(500).json({ error: errorMessage, errorTime })
+
+}
 
 
 app.use(express.json())
 app.use(cors())
 
+
 app.get("/quiz", (req, res) => {
     db.query("SELECT * FROM quiz", (err, result) => {
         if (err) {
-            // Gérez l'erreur de requête à la base de données
-            console.error(err);
-            res.status(500).json({ error: "Erreur lors de la récupération des quiz" });
+            errorHandling(res,err,"Erreur lors de la récupération des quiz");
         } else {
-
-            // Envoyer la liste des quiz en réponse
             res.json(result);
         }
     })
 });
 
-app.get("/quiz/:slug", (req,res)=>{
+app.get("/quiz/:slug", (req, res) => {
     console.log(req.params.slug)
-    db.query(`SELECT * FROM quiz WHERE slug = '${req.params.slug}'`,(err,result)=>{
-        if(err){
-            console.error(err);
-            res.status(500).json({ error: "Erreur lors de la récupération d'un quiz"})
+    db.query('SELECT * FROM quiz WHERE slug = ?', [req.params.slug], (err, result) => {
+        if (err) {
+            errorHandling(res,err,"Erreur lors de la récupération d'un quiz");
+
         } else {
             res.json(result[0])
         }
     })
-})
+});
+
+
 
 
 app.listen(8800, () => {
