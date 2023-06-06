95ea82b3e36d53c4a769716fd6f549f688a2b9df
sueys <sueys@live.fr>
2023-06-06 11:20:53 +0200

    optimisation en mysql2

--- db.js
+++ db.js
@@ -1,19 +1,14 @@
 import dotenv from 'dotenv';
-import mysql from 'mysql';
+import mysql from 'mysql2/promise';
 
 dotenv.config();
-export const db = mysql.createConnection({
+
+export const db = await mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    charset:"utf8mb4"
+    charset: 'utf8mb4'
 });
 
-db.connect((err)=>{
-    if(err){
-        console.error('Erreur de connexion à la base de données MySQL :', err);
-    } else {
-        console.log('Connecté à la base de données MySQL');
-    }
-});
+console.log('Connecté à la base de données MySQL');
