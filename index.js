2c6f1e05c733c46fe5f8d5db26a4df682a8982c7
sueys <sueys@live.fr>
2023-06-06 11:25:06 +0200

    add bcrypt and optimization code with execute

--- index.js
+++ index.js
@@ -1,61 +1,60 @@
-import express from "express";
-import passport from "passport";
-import cors from "cors"
-import { db } from "./db.js"
-import { sessionMiddleWare } from "./config/session.js";
-import { ConfigPassport } from "./passport-config.js";
+import express from 'express';
+import passport from 'passport';
+import cors from 'cors';
+import { db } from './db.js';
+import { sessionMiddleWare } from './config/session.js';
+import { configPassport } from './config/passport.js';
+import bcrypt from 'bcrypt';
+import { hashPassword } from './utils/hashPassword.js';
 
-const errorHandling = (res, error, errorMessage = "an error has occurred") => {
+const errorHandling = (res, error, errorMessage = 'An error has occurred') => {
     const errorTime = new Date().getTime();
     console.error(errorTime, error);
-    res.status(500).json({ error: errorMessage, errorTime })
-}
+    res.status(500).json({ error: errorMessage, errorTime });
+};
 
-const app = express()
-ConfigPassport()
-app.use(sessionMiddleWare)
-app.use(express.json())
-app.use(cors())
+const app = express();
+configPassport();
+app.use(sessionMiddleWare);
+app.use(express.json());
+app.use(cors());
 
-app.get("/quiz", (req, res) => {
-    db.query("SELECT * FROM quiz", (err, result) => {
-        if (err) {
-            errorHandling(res, err, "Erreur lors de la récupération des quiz");
-        } else {
-            res.json(result);
-        }
-    })
+app.get('/quiz', async (req, res) => {
+    try {
+        const [result] = await db.execute('SELECT * FROM quiz');
+        res.json(result);
+    } catch (err) {
+        errorHandling(res, err, 'Erreur lors de la récupération des quiz');
+    }
 });
 
-app.get("/quiz/:slug", (req, res) => {
-    console.log(req.params.slug)
-    db.query('SELECT * FROM quiz WHERE slug = ?', [req.params.slug], (err, result) => {
-        if (err) {
-            errorHandling(res, err, "Erreur lors de la récupération d'un quiz");
-
-        } else {
-            res.json(result[0])
-        }
-    })
+app.get('/quiz/:slug', async (req, res) => {
+    try {
+        const [result] = await db.execute('SELECT * FROM quiz WHERE slug = ?', [req.params.slug]);
+        res.json(result[0]);
+    } catch (err) {
+        errorHandling(res, err, "Erreur lors de la récupération d'un quiz");
+    }
 });
 
-app.post("/login", (req, res, next) => {
-    passport.authenticate("local", (err, user) => {
+app.post('/login', (req, res, next) => {
+    passport.authenticate('local', (err, user) => {
         if (err) {
             return next(err);
         }
         if (!user) {
-            return res.status(401).json({ message: "Invalid credentials" });
+            return res.status(401).json({ message: 'Invalid credentials' });
         }
         req.logIn(user, (err) => {
             if (err) {
                 return next(err);
             }
-            return res.json({ message: "Authentication successful" });
+            return res.json({ message: 'Authentication successful' });
         });
     })(req, res, next);
 });
 
+
 app.listen(8800, () => {
-    console.log("Connected")
-})
+    console.log('Connected');
+});
