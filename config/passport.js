551cbecf13a1906359d4ec774a6427eaed34a5cc
sueys <sueys@live.fr>
2023-06-06 11:25:53 +0200

    same passport but changed name

---
+++ config/passport.js
@@ -0,0 +1,54 @@
+import { Strategy as LocalStrategy } from "passport-local";
+import passport from "passport";
+import bcrypt from "bcrypt";
+import { db } from "../db.js";
+
+const authenticateUser = (username, password, done) => {
+    const query = "SELECT * FROM users WHERE username = ?";
+    db.execute(query, [username])
+        .then(([rows]) => {
+            if (!rows.length) {
+                return done(null, false, { message: "Invalid credentials" });
+            }
+            const user = rows[0];
+
+            bcrypt.compare(password, user.password, (err, isMatch) => {
+                if (err) {
+                    return done(err);
+                }
+                if (!isMatch) {
+                    return done(null, false, { message: "Invalid credentials" });
+                }
+
+                return done(null, user);
+            });
+        })
+        .catch((error) => {
+            return done(error);
+        });
+};
+
+const serializeUser = (user, done) => {
+    done(null, user.id);
+};
+
+const deserializeUser = (id, done) => {
+    const query = "SELECT * FROM users WHERE id = ?";
+    db.execute(query, [id])
+        .then(([rows]) => {
+            if (!rows.length) {
+                throw new Error("User not found");
+            }
+            const user = rows[0];
+            return done(null, user);
+        })
+        .catch((error) => {
+            return done(error);
+        });
+};
+
+export const configPassport = () => {
+    passport.use(new LocalStrategy(authenticateUser));
+    passport.serializeUser(serializeUser);
+    passport.deserializeUser(deserializeUser);
+};
