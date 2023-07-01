a8ca845d1e0f1d19543ab9a771cc9b8c8397040a
sueys <sueys@live.fr>
2023-07-01 11:55:02 +0200

    refactor : all then change by async/await

--- config/passport.js
+++ config/passport.js
@@ -4,32 +4,33 @@ import bcrypt from "bcryptjs";
 import { db } from "../Lib/db.js";
 import jwt from 'jsonwebtoken';
 
-const authenticateUser = (email, password, done) => {
+const authenticateUser = async (email, password, done) => {
     console.log('Authenticating user...');
-    const query = "SELECT * FROM users WHERE email = ?";
-    db.execute(query, [email])
-        .then(([rows]) => {
-            if (!rows.length) {
+    try {
+        const query = "SELECT * FROM users WHERE email = ?";
+        const [rows] = await db.execute(query, [email]);
+
+        if (!rows.length) {
+            return done(null, false, { message: "Invalid credentials" });
+        }
+
+        const user = rows[0];
+
+        bcrypt.compare(password, user.password, (err, isMatch) => {
+            console.log('Comparing passwords...');
+            console.log('isMatch:', isMatch);
+            if (err) {
+                return done(err);
+            }
+            if (!isMatch) {
                 return done(null, false, { message: "Invalid credentials" });
             }
-            const user = rows[0];
 
-            bcrypt.compare(password, user.password, (err, isMatch) => {
-                console.log('Comparing passwords...');
-                console.log('isMatch:', isMatch);
-                if (err) {
-                    return done(err);
-                }
-                if (!isMatch) {
-                    return done(null, false, { message: "Invalid credentials" });
-                }
-
-                return done(null, user);
-            });
-        })
-        .catch((error) => {
-            return done(error);
+            return done(null, user);
         });
+    } catch (error) {
+        return done(error);
+    }
 };
 
 const serializeUser = (user, done) => {
@@ -37,27 +38,28 @@ const serializeUser = (user, done) => {
     done(null, user.id);
 };
 
-const deserializeUser = (id, done) => {
+const deserializeUser = async (id, done) => {
     console.log('Deserializing user...');
-    const query = "SELECT * FROM users WHERE id = ?";
-    db.execute(query, [id])
-        .then(([rows]) => {
-            if (!rows.length) {
-                throw new Error("User not found");
-            }
-            const user = rows[0];
-            return done(null, user);
-        })
-        .catch((error) => {
-            return done(error);
-        });
+    try {
+        const query = "SELECT * FROM users WHERE id = ?";
+        const [rows] = await db.execute(query, [id]);
+
+        if (!rows.length) {
+            throw new Error("User not found");
+        }
+
+        const user = rows[0];
+        return done(null, user);
+    } catch (error) {
+        return done(error);
+    }
 };
+
 
 export const generateToken = (user) => {
 
     const payload = {
         id: user.id,
-        email: user.email,
     };
     console.log(payload)
     const token = jwt.sign(payload, process.env.JWT_SECRET, {
