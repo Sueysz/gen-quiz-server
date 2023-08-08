4b9efb83998f098542b0615187325d3c3fcf8203
sueys <sueys@live.fr>
2023-08-08 17:00:04 +0200

    add table score

--- sql/tableCreate.sql
+++ sql/tableCreate.sql
@@ -1,49 +1,61 @@
 -- Créer la base de données 'genquiz'
 CREATE DATABASE genquiz;
 
--- Utiliser la base de données 'genquiz'
-USE genquiz;
-
--- Créer la table 'users'
-CREATE TABLE users (
-   id INT NOT NULL AUTO_INCREMENT,
-   username VARCHAR(45) NOT NULL,
-   email VARCHAR(255) NOT NULL,
-   password VARCHAR(255) NOT NULL,
-   PRIMARY KEY (id)
+CREATE TABLE `users` (
+   `id` int PRIMARY KEY AUTO_INCREMENT,
+   `username` varchar(255) NOT NULL,
+   `email` varchar(255) NOT NULL,
+   `password` varchar(255) NOT NULL
 );
 
--- Créer la table 'quiz'
-CREATE TABLE quiz (
-   id INT NOT NULL AUTO_INCREMENT,
-   color VARCHAR(255) NOT NULL,
-   slug VARCHAR(255),
-   questions json,
-   PRIMARY KEY (id)
+CREATE TABLE `quiz` (
+   `id` int PRIMARY KEY AUTO_INCREMENT,
+   `color` varchar(255) NOT NULL,
+   `slug` varchar(255),
+   `questions` json,
+   `creator_id` int
 );
 
-CREATE TABLE `genquiz`.`category` (
-   `id` INT NOT NULL AUTO_INCREMENT,
-   `name` VARCHAR(45) NOT NULL,
-   PRIMARY KEY (`id`));
+CREATE TABLE `category` (
+   `id` int PRIMARY KEY AUTO_INCREMENT,
+   `name` varchar(255) NOT NULL
+);
+
+CREATE TABLE `history` (
+   
+);
+
+CREATE TABLE `categories_quiz` (
+   `id` int,
+   `categories_id` int,
+   `quiz_id` int
+)ENGINE=INNODB;
+
+CREATE TABLE `score` (
+   `id` int,
+   `user_id` int,
+   `quiz_id` int,
+   `score` int,
+   `created_at` timeStamp
+);
+
+ALTER TABLE `categories_quiz` ADD FOREIGN KEY (`quiz_id`) REFERENCES `quiz` (`id`);
+
+ALTER TABLE `score` ADD FOREIGN KEY (`quiz_id`) REFERENCES `quiz` (`id`);
+
+ALTER TABLE `categories_quiz` ADD FOREIGN KEY (`categories_id`) REFERENCES `category` (`id`);
+
+ALTER TABLE `quiz` ADD FOREIGN KEY (`creator_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `score` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 
-CREATE TABLE history (
-   id INT NOT NULL AUTO_INCREMENT,
-
-)
-
-CREATE TABLE categories_quiz (
-   id INT,
-   categories_id INT,
+CREATE TABLE score (
+   id INT PRIMARY KEY AUTO_INCREMENT,
+   user_id INT,
    quiz_id INT,
-   FOREIGN KEY (categories_id)
-      REFERENCES categories(id)
-      ON UPDATE CASCADE
-      ON DELETE CASCADE,
-	FOREIGN KEY (quiz_id)
-		REFERENCES quiz(id)
-      ON UPDATE CASCADE
-      ON DELETE CASCADE
-) ENGINE=INNODB;
-
+   score INT,
+   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+   FOREIGN KEY (user_id) REFERENCES user(id),
+   FOREIGN KEY (quiz_id) REFERENCES quiz(id)
+);
